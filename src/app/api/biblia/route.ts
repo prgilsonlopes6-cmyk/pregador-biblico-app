edb7ef8597fd3c4cf3844864549d5634726727bf
Gilson Araujo | Essentia Energia <gilson.araujo@essentiaenergia.com.br>
2026-04-29 08:31:34 -0300

    Forcando APIs como dinamicas para garantir build da Vercel

--- src/app/api/biblia/route.ts
+++ src/app/api/biblia/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const busca = searchParams.get("busca");
